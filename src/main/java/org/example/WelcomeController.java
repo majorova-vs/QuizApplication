a11955740e13fd2378be7485db223595b995d292
majorova-vs <Valerya929@gmail.com>
2020-07-29 17:27:48 +0300

    project created and configured

---
+++ src/main/java/org/example/WelcomeController.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String sayHello() {
+        return "Welcome.html";
+    }
+}
